283c80d818802ef9da59fd404510bd24389a0390
Sazzad Hossain <sazz.pranto96@gmail.com>
2021-04-15 03:20:33 +0600

    auth

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');  // bodyParser is a middleware 
+const cookieSession = require('cookie-session');  // cookie-session is a middleware function
+
 const usersRepo = require('./repositories/users.js');  // contains UsersRepository
+const users = require('./repositories/users.js');
 
 const app = express();
 // urlencoded method takes out any html form data and make an object
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    cookieSession({
+        keys: ['OglplzxYqD3fKLadkj89fs7mv']
+    })
+);
 
 //this code is similar to how bodyParser code is written
-// // middleware funciton->
+// // middleware function->
 // const bodyParser = (req, res, next) => {
 //     if(req.method === 'POST') {
 //         req.on('data', (data) => {
@@ -25,9 +33,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     }
 // };
 
-app.get('/', (req, res) => {
+// signup page
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
+            Your ID is: ${req.session.userId}
             <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password" placeholder="password" />
@@ -38,12 +48,15 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', async (req, res) => { // async keyword is used cause usersRepo has await
+// post request handler for the form submission from signup page
+app.post('/signup', async (req, res) => { // async keyword is used cause usersRepo has await
     const { email, password, passwordConfirmation } = req.body;
+
     /* getOneBy requires a filter object as param, here the email
     property is given to see if this email is already in use or not
     getOneBy has some async functionality, so await is used */
     const existingUser = await usersRepo.getOneBy({ email: email });
+
     if(existingUser) {
         // if there's already an user with this email, send this
         return res.send('Email is already in use!');
@@ -53,9 +66,70 @@ app.post('/', async (req, res) => { // async keyword is used cause usersRepo has
         return res.send('Passwords must match');
     }
 
+    // create a user
+    const user = await usersRepo.create({ email: email, password: password });
+
+    /* req.session is added by the cookie-session library, it is basically an object and also 
+    more properties can be added to this object. Here userId is added. If any custom property like this
+    is added and a response is sent, the cookie-session library takes a look at the req.session object
+    and checks if any information(here property) is changed, it takes that info and encode it into a simple string
+    attach it as the outgoing response as the cookie that should be stored on the user's browser
+    Here, we are adding the id of the signed in user inside the userId property, which will eventually added to the 
+    user's browser as a cookie, all handled by the cookie-session module */
+    req.session.userId = user.id;  
+    /* cookie-session adds a session property to the incoming request object when a response is sent
+    to recognize a user, here the user's id is being stored to a cookie */
     res.send('Account created!!!');
 });
 
+// signout page
+app.get('/signout', (req, res) => {
+    // erase the current user's session
+    req.session = null;
+    res.send('You are signed out');
+});
+
+// signin page
+app.get('/signin' ,(req, res) => {
+    // signin form
+    res.send(`
+        <div>
+            <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+            </form>
+        </div>
+    `);
+});
+
+// post request handler for the signin form from signin page
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    /* getOneBy requires a filter object as param, here the email
+    property is given to see if this email is already in use or not
+    getOneBy has some async functionality, so await is used */
+    const user = await usersRepo.getOneBy({ email: email });
+    if(!user) {
+        // if there's no user with this email, send this
+        return res.send('Email not found!');
+    }
+    
+    // password validation, will be true if password matches the stored one
+    const validPassword = await usersRepo.comparePassword(
+        user.password,
+        password
+    );
+
+    if(!validPassword) {
+        // if password is wrong, send this
+        return res.send('Wrong password!');
+    }
+
+    // if user's email and password are valid, set a cookie for the user using its id
+    req.session.userId = user.id;
+    res.send('You are signed in');
+})
 app.listen(3000, () => {
     console.log('Listening');
 });
--- repositories/users.js
+++ repositories/users.js
@@ -4,6 +4,12 @@
 
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+/* promisify takes crypto.scrypt and returns a promise based version of if
+normally crypto.scrypt takes a callback in it which would be executed later
+to avoid this, using promisify is a good approach */
+const scrypt = util.promisify(crypto.scrypt);
+
 
 class UsersRepository {
     constructor(filename) {
@@ -40,11 +46,33 @@ class UsersRepository {
     // create a new user
     async create(attributes) {
         attributes.id = this.randomId() // adding id property to attributes object and assigning a randomly generated id
+
+        const salt = crypto.randomBytes(8).toString('hex'); // generating salt for password
+        const buffer = await scrypt(attributes.password, salt, 64);  // scrypt takes the password, salts it and returns the hashed version in buffer, so it needs to be converted into string
+        
         const records = await this.getAll();  // get the previous list of users, since getAll() is an async method, await is used here
-        records.push(attributes);  // push the new user to the records
+
+        /* making a new record of a user, replacing the plain password with the hashed one
+        the salt is added after the period (.) sign, so that it can be separated later with split('.') */
+        const newRecord = {
+            ...attributes,
+            password: `${buffer.toString('hex')}.${salt}`
+        }
+        records.push(newRecord); // push the new user to the records
 
         // send the records to the writeALl() method
         await this.writeAll(records);
+        return newRecord;  // returning the new user's attributes along with the randomly generated id
+    }
+
+    // check if the passwords are same
+    async comparePassword(savedPass, suppliedPass) {
+        // savedPass -> password saved in the database (hashed.salt)
+        // suppliedPass -> password given by the user while logging in
+
+        const [hashed, salt] = savedPass.split('.'); // splitting hashed and salt and creating two variable using array destructuring
+        const hashedSuppliedBuff = await scrypt(suppliedPass, salt, 64); // hashing and salting the supplied password, returns buffer
+        return hashed === hashedSuppliedBuff.toString('hex'); // return true or false based on equality
     }
 
     // write to the file(here, users.json) with updated users records
